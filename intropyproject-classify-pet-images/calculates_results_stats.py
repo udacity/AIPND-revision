3ebd7784d90a725c758e381675df1fea5354629c
nickyrasch <nickynrasch@gmail.com>
2020-06-06 19:42:14 -0700

    better syntax

--- intropyproject-classify-pet-images/calculates_results_stats.py
+++ intropyproject-classify-pet-images/calculates_results_stats.py
@@ -71,19 +71,19 @@ def calculates_results_stats(results_dic):
     # Replace None with the results_stats_dic dictionary that you created with 
     # this function
 
-    results_stats_dic = {}
-
-    results_stats_dic['n_images'] = len(results_dic)
-    results_stats_dic['n_dogs_img'] = 0
-    results_stats_dic['n_notdogs_img'] = 0
-    results_stats_dic['n_match'] = 0
-    results_stats_dic['n_correct_dogs'] = 0
-    results_stats_dic['n_correct_notdogs'] = 0
-    results_stats_dic['n_correct_breed'] = 0
-    results_stats_dic['pct_match'] = 0.0
-    results_stats_dic['pct_correct_dogs'] = 0.0
-    results_stats_dic['pct_correct_breed'] = 0.0
-    results_stats_dic['pct_correct_notdogs'] = 0.0
+    results_stats_dic = {
+        'n_images': len(results_dic),
+        'n_dogs_img': 0,
+        'n_notdogs_img': 0,
+        'n_match': 0,
+        'n_correct_dogs': 0,
+        'n_correct_notdogs': 0,
+        'n_correct_breed': 0,
+        'pct_match': 0.0,
+        'pct_correct_dogs': 0.0,
+        'pct_correct_breed': 0.0,
+        'pct_correct_notdogs': 0.0,
+    }
 
     for key in results_dic:
         if results_dic[key][3] == 1:
